55090555e15a74bcb170f7deecd31f2d1c7bd572
SchizoAA <kyle.erickson15@gmail.com>
2020-02-24 23:18:40 -0700

    Completed Tic Tac Toe game and tests

---
+++ WTW_InterviewChallenge/WTW_InterviewChallenge.Test/TicTacToeTests.cs
@@ -0,0 +1,220 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WTW_InterviewChallenge.Game;
+
+namespace WTW_InterviewChallenge.Test
+{
+    [TestClass]
+    public class TicTacToeTests
+    {
+        private TicTacToe gameboard_3x3 = new TicTacToe(3);
+        private TicTacToe gameboard_4x4 = new TicTacToe(4);
+        private TicTacToe gameboard_6x6 = new TicTacToe(6);
+        private TicTacToe gameboard_12x12 = new TicTacToe(12);
+        private TicTacToe gameboard_100x100 = new TicTacToe(100);
+
+        [TestMethod]
+        public void Test_TicTacToe_InvalidPlayer()
+        {
+            gameboard_3x3 = new TicTacToe(3);
+            Assert.ThrowsException<InvalidOperationException>(() => gameboard_3x3.PlacePiece(0, 0, 3));
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_RowOutOfRange()
+        {
+            gameboard_3x3 = new TicTacToe(3);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => gameboard_3x3.PlacePiece(5, 0, 1));
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_ColumnOutOfRange()
+        {
+            gameboard_3x3 = new TicTacToe(3);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => gameboard_3x3.PlacePiece(0, 5, 1));
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_InvalidMove()
+        {
+            gameboard_3x3 = new TicTacToe(3);
+            gameboard_3x3.PlacePiece(0, 0, 1);
+            Assert.ThrowsException<InvalidOperationException>(() => gameboard_3x3.PlacePiece(0, 0, 2),"A player has already claimed this space.");
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_3x3_PlacePiece()
+        {
+            gameboard_3x3 = new TicTacToe(3);
+            var result = gameboard_3x3.PlacePiece(2, 0, 1);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_3x3_P1Win_Vertical()
+        {
+            gameboard_3x3 = new TicTacToe(3);
+            gameboard_3x3.PlacePiece(0, 0, 1);
+            gameboard_3x3.PlacePiece(1, 0, 1);
+            var result = gameboard_3x3.PlacePiece(2, 0, 1);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_3x3_P1Win_Horizontal()
+        {
+            gameboard_3x3 = new TicTacToe(3);
+            gameboard_3x3.PlacePiece(0, 0, 1);
+            gameboard_3x3.PlacePiece(0, 1, 1);
+            var result = gameboard_3x3.PlacePiece(0, 2, 1);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_3x3_P1Win_Diagonal()
+        {
+            gameboard_3x3 = new TicTacToe(3);
+            gameboard_3x3.PlacePiece(0, 0, 1);
+            gameboard_3x3.PlacePiece(1, 1, 1);
+            var result = gameboard_3x3.PlacePiece(2, 2, 1);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_3x3_P2Win_Vertical()
+        {
+            gameboard_3x3 = new TicTacToe(3);
+            gameboard_3x3.PlacePiece(0, 0, 2);
+            gameboard_3x3.PlacePiece(1, 0, 2);
+            var result = gameboard_3x3.PlacePiece(2, 0, 2);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_3x3_P2Win_Horizontal()
+        {
+            gameboard_3x3 = new TicTacToe(3);
+            gameboard_3x3.PlacePiece(0, 0, 2);
+            gameboard_3x3.PlacePiece(0, 1, 2);
+            var result = gameboard_3x3.PlacePiece(0, 2, 2);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_3x3_P2Win_Diagonal()
+        {
+            gameboard_3x3 = new TicTacToe(3);
+            gameboard_3x3.PlacePiece(0, 0, 2);
+            gameboard_3x3.PlacePiece(1, 1, 2);
+            var result = gameboard_3x3.PlacePiece(2, 2, 2);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_3x3_P2Win_Simulated()
+        {
+            gameboard_3x3 = new TicTacToe(3);
+            gameboard_3x3.PlacePiece(0, 1, 1);
+            gameboard_3x3.PlacePiece(2, 0, 2);
+            gameboard_3x3.PlacePiece(0, 0, 1);
+            gameboard_3x3.PlacePiece(0, 2, 2);
+            gameboard_3x3.PlacePiece(1, 0, 1);
+            var result = gameboard_3x3.PlacePiece(1, 1, 2);
+            Assert.AreEqual(2, result);
+            Assert.ThrowsException<InvalidOperationException>(() => gameboard_3x3.PlacePiece(2, 1, 1), "The game has concluded.");
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_4x4_P1Win_Simulated()
+        {
+            gameboard_4x4 = new TicTacToe(4);
+            gameboard_4x4.PlacePiece(0, 1, 1);
+            gameboard_4x4.PlacePiece(2, 0, 2);
+            gameboard_4x4.PlacePiece(0, 0, 1);
+            gameboard_4x4.PlacePiece(2, 1, 2);
+            gameboard_4x4.PlacePiece(0, 3, 1);
+            gameboard_4x4.PlacePiece(1, 1, 2);
+            int result = gameboard_4x4.PlacePiece(0, 2, 1);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_4x4_TieGame()
+        {
+            gameboard_4x4 = new TicTacToe(4);
+            gameboard_4x4.PlacePiece(0, 0, 1);
+            gameboard_4x4.PlacePiece(0, 1, 2);
+            gameboard_4x4.PlacePiece(0, 2, 1);
+            gameboard_4x4.PlacePiece(1, 2, 2);
+            gameboard_4x4.PlacePiece(1, 0, 1);
+            gameboard_4x4.PlacePiece(1, 1, 2);
+            gameboard_4x4.PlacePiece(2, 2, 1);
+            gameboard_4x4.PlacePiece(2, 0, 2);
+            int result = gameboard_4x4.PlacePiece(2, 1, 1);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_4x4_P1Win_Diagonal()
+        {
+            gameboard_4x4 = new TicTacToe(4);
+            gameboard_4x4.PlacePiece(0, 0, 1);
+            gameboard_4x4.PlacePiece(1, 1, 1);
+            gameboard_4x4.PlacePiece(2, 2, 1);
+            int result = gameboard_4x4.PlacePiece(3, 3, 1);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_6x6_P1Win()
+        {
+            gameboard_6x6 = new TicTacToe(6);
+            gameboard_6x6.PlacePiece(0, 0, 1);
+            gameboard_6x6.PlacePiece(0, 1, 1);
+            gameboard_6x6.PlacePiece(0, 2, 1);
+            gameboard_6x6.PlacePiece(0, 3, 1);
+            gameboard_6x6.PlacePiece(0, 4, 1);
+            var result = gameboard_6x6.PlacePiece(0, 5, 1);
+            Assert.AreEqual(1, result);
+            Assert.ThrowsException<InvalidOperationException>(() => gameboard_6x6.PlacePiece(2, 1, 1), "The game has concluded.");
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_12x12_P2Win()
+        {
+            gameboard_12x12 = new TicTacToe(12);
+            gameboard_12x12.PlacePiece(0, 0, 2);
+            gameboard_12x12.PlacePiece(1, 1, 2);
+            gameboard_12x12.PlacePiece(2, 2, 2);
+            gameboard_12x12.PlacePiece(3, 3, 2);
+            gameboard_12x12.PlacePiece(4, 4, 2);
+            gameboard_12x12.PlacePiece(5, 5, 2);
+            gameboard_12x12.PlacePiece(6, 6, 2);
+            gameboard_12x12.PlacePiece(7, 7, 2);
+            gameboard_12x12.PlacePiece(8, 8, 2);
+            gameboard_12x12.PlacePiece(9, 9, 2);
+            gameboard_12x12.PlacePiece(10, 10, 2);
+            var result = gameboard_12x12.PlacePiece(11, 11, 2);
+            Assert.AreEqual(2, result);
+            Assert.ThrowsException<InvalidOperationException>(() => gameboard_12x12.PlacePiece(2, 1, 1), "The game has concluded.");
+        }
+
+        [TestMethod]
+        public void Test_TicTacToe_100x100_P1Win()
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+
+            gameboard_100x100 = new TicTacToe(100);
+            for( int i = 0; i < 99; i++ )
+            {
+                gameboard_100x100.PlacePiece(i, 0, 1);
+            }
+            var result = gameboard_100x100.PlacePiece(99, 0, 1);
+            watch.Stop();
+
+            Assert.IsTrue(watch.ElapsedMilliseconds <= 1000);
+            Assert.AreEqual(1, result);
+            Assert.ThrowsException<InvalidOperationException>(() => gameboard_100x100.PlacePiece(2, 1, 1), "The game has concluded.");
+        }
+    }
+}
---
+++ WTW_InterviewChallenge/WTW_InterviewChallenge/Game/TicTacToe.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WTW_InterviewChallenge.Game
+{
+    /// <summary>
+    /// TicTacToe game class
+    /// </summary>
+    public class TicTacToe
+    {
+        private readonly int[] _rows;
+        private readonly int[] _columns;
+        private readonly int _gridSize;
+        private int _diagonalRight;
+        private int _diagonalLeft;
+        private int _moves;
+        private readonly HashSet<Tuple<int, int>> _claimed;
+        public int Winner { get; private set; }
+
+        /// <summary>
+        /// TicTacToe constructor
+        /// </summary>
+        /// <param name="n">Grid size nxn</param>
+        public TicTacToe(int n)
+        {
+            _gridSize = n;
+            _rows = new int[n];
+            _columns = new int[n];
+            _claimed = new HashSet<Tuple<int, int>>();
+            _moves = 0;
+            Winner = 0;
+        }
+
+        /// <summary>
+        /// Places a game piece on a grid cell for the given player.
+        /// </summary>
+        /// <param name="row">cell row</param>
+        /// <param name="col">cell column</param>
+        /// <param name="player">player placing piece</param>
+        /// <returns> player number if game winner, 0 otherwise.</returns>
+        public int PlacePiece( int row, int col, int player )
+        {
+            var coords = new Tuple<int, int>(row, col);
+            if( Winner != 0 )
+            {
+                throw new InvalidOperationException("The game has concluded.");
+            }
+            if( player == 0 || player > 2 )
+            {
+                throw new InvalidOperationException($"Invalid player: {player}. Player must be either 1 or 2.");
+            }
+            if( row > _gridSize || col > _gridSize )
+            {
+                throw new ArgumentOutOfRangeException("Move exceeds gameboard boundaries.");
+            }
+            if( _claimed.Contains(coords) )
+            {
+                throw new InvalidOperationException("A player has already claimed this space.");
+            }
+            _claimed.Add(coords);
+            _moves++;            
+            var move = player == 1 ? 1 : -1; //Player one positive, player 2 negative.
+
+            //We don't care exactly which cells a player has. We can tell if they have won if the absolute value sum of pieces in a row/column/diagonal equals n.
+            //for example: When n=4, p1 wins when a row, column, or diagonal has a sum of 4. P2 wins if a row, column, or diagonal has a sum of -4.
+            _rows[row] += move; 
+            _columns[col] += move;
+            if( row == col )
+            {
+                _diagonalRight += move;
+            }
+            if( col == _gridSize - row - 1 )
+            {
+                _diagonalLeft += move;
+            }
+
+            if( CheckForWinner( row, col ) )
+            {
+                Winner = player;
+                return player;
+            }
+            return 0;
+        }
+
+        private bool CheckForWinner( int row, int col )
+        {
+            if( _moves >= ( _gridSize * _gridSize ))
+            {
+                Winner = -1;
+                return false;
+            }
+            if ( Math.Abs(_rows[row]) == _gridSize || Math.Abs(_columns[col]) == _gridSize || Math.Abs(_diagonalRight) == _gridSize || Math.Abs(_diagonalLeft) == _gridSize )
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
